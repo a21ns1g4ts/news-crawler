6c0a35cf630b534b2cc6a28578218b2787f5aff6
Atiladanvi <atila.danvi@outlook.com>
2019-08-24 18:24:42 -0300

    Adiciona comentários em testes

--- tests/DiscoveryAylienCategoryTest.php
+++ tests/DiscoveryAylienCategoryTest.php
@@ -3,15 +3,28 @@
 
 use App\Discoverers\DiscoveryAylienCategory;
 
+/**
+ * Class DiscoveryAylienCategoryTest
+ */
 class DiscoveryAylienCategoryTest extends TestCase
 {
 
+    /**
+     * Text to analyze
+     *
+     * @var string
+     */
     static $content = 'Jovens em situação de vulnerabilidade social têm a oportunidade 
      de participar do programa jovem aprendiz com a parceria mediada pelo Senac.Confira
      todas as fotos na Galeria O Senac Pará, através de seu Núcleo de Educação Profissional
      em Marabá (NEP Marabá), está recebendo a primeira turma de jovens aprendizes contratados
      por meio da Aprendizagem na forma alternativa. O formato…';
 
+    /**
+     * Test discovery
+     *
+     * @param void
+     */
     public function testDiscoveryCategory(){
         $discovery =  new DiscoveryAylienCategory(self::$content);
         $discovery->detect();
